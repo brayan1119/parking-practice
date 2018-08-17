e94ab10508bbfa49ae325729d9894c4ca58dbe94
Brayan Gallego <brayan.gallego@ceiba.com.co>
2018-08-17 14:09:28 -0500

    Commit inicial con los primeros ajustes del proyecto

---
+++ src/main/java/co/com/parking/parkingpractice/ParkingPracticeApplication.java
@@ -0,0 +1,12 @@
+package co.com.parking.parkingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingPracticeApplication.class, args);
+	}
+}
