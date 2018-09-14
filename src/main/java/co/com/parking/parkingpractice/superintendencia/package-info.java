9eacd372b235f576e9fd34de267355ab482e9b70
Brayan Gallego <brayan.gallego@ceiba.com.co>
2018-09-14 11:34:55 -0500

    eliminar superintendia

--- src/main/java/co/com/parking/parkingpractice/superintendencia/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://action.trm.services.generic.action.superfinanciera.nexura.sc.com.co/")
-package co.com.parking.parkingpractice.superintendencia;
